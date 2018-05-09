87a4d71ed4bc261962c9b2daf00050e32d7823dd
Leif Warner <abimelech@gmail.com>
2018-05-09 14:51:58 -0700

    Added .cabal, cleanup, modernize TH invocations, deriving

--- App.hs
+++ App.hs
@@ -1,21 +1,20 @@
-{-# LANGUAGE DeriveDataTypeable, DeriveGeneric, TemplateHaskell, DataKinds, TypeFamilies, TypeOperators, TypeApplications, FlexibleContexts, FlexibleInstances, RankNTypes, ConstraintKinds #-}
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric, DeriveAnyClass, TemplateHaskell, DataKinds, TypeFamilies, TypeOperators, TypeApplications, FlexibleContexts, FlexibleInstances, RankNTypes, ConstraintKinds, OverloadedStrings #-}
 module App where
 
-import           Control.Monad.Reader
-import           Control.Monad.State
+import Control.Monad.Reader
+import Control.Monad.State
 import Control.Monad.Error.Class (MonadError, liftEither)
---import Control.Monad.Except (MonadError, liftEither, ExceptT(ExceptT))
 import Data.Either.Combinators (maybeToRight)
-import           Data.Acid
-import           Data.SafeCopy
-import           Data.Typeable
+import Data.Acid
+import Data.SafeCopy
+import Data.Typeable
 import GHC.Generics
 
---import Servant.API
+import Data.Text (Text())
+
 import Servant
 import Servant.Docs
 import Data.Aeson.Types
---import Data.Aeson.Compat
 import Network.Wai.Handler.Warp (run)
 
 import Control.Exception.Base (bracket)
@@ -23,38 +22,35 @@ import Control.Exception.Base (bracket)
 import qualified Data.Map.Strict as Map
 import Data.Functor (($>))
 
+-- From http://hackage.haskell.org/package/natural-transformation-0.4/docs/Control-Natural.html#t:-126--62-
+infixr 0 ~>
+type f ~> g = forall x. f x -> g x
 
 maybeError :: MonadError e m => e -> Maybe a -> m a
 maybeError err = liftEither . maybeToRight err
 
-type Name = String
+type Name = Text
 
 data Person = Person {
   name :: Name,
   age :: Int
-} deriving (Show, Typeable, Generic)
+} deriving (Show, Typeable, Generic, ToJSON, FromJSON)
 
-instance ToJSON Person
-instance FromJSON Person
+samplePerson = Person "Bob" 40
 
 instance ToSample Person where
-  toSamples _ = singleSample $ Person "Bob" 40
-
---person = Person "Leif" 38
---people = [person]
+  toSamples _ = singleSample samplePerson
 
 newtype PeopleDb = PeopleDb { peopleMap :: Map.Map Name Person }
 
-$(deriveSafeCopy 0 'base ''Person)
-$(deriveSafeCopy 0 'base ''PeopleDb)
+deriveSafeCopy 0 'base ''Person
+deriveSafeCopy 0 'base ''PeopleDb
+
+lookupPerson' :: MonadReader PeopleDb m => Name -> m (Maybe Person)
+lookupPerson' name = asks $ Map.lookup name . peopleMap
 
 lookupPerson :: Name -> Query PeopleDb (Maybe Person)
-lookupPerson = foo
---lookupPerson name = asks $ Map.lookup name . peopleMap
-
-foo :: MonadReader PeopleDb m => Name -> m (Maybe Person)
-foo name = asks $ Map.lookup name . peopleMap
---lookupPerson name = asks (\(PeopleDb peopleMap) -> Map.lookup name peopleMap)
+lookupPerson = lookupPerson'
 
 allPeople :: Query PeopleDb [Person]
 allPeople = asks $ Map.elems . peopleMap
@@ -68,10 +64,8 @@ setPerson' name person = do
 setPerson :: Name -> Person -> Update PeopleDb Bool
 setPerson = setPerson'
 
-$(makeAcidic ''PeopleDb ['lookupPerson, 'allPeople, 'setPerson])
+makeAcidic ''PeopleDb ['lookupPerson, 'allPeople, 'setPerson]
 
-
---type PeopleIndex = "people" :> Get '[JSON] [Person]
 type NameSegment = Capture "name" Name
 
 type PeopleIndex = Get '[JSON] [Person]
@@ -87,42 +81,22 @@ type PeopleAPI = "people" :> (
     UpdateSpecificPerson
   )
 
---foo :: MonadIO m => m [Person]
---foo = pure people
-
---bar :: (MonadIO m, MonadError ServantErr m) => Name -> m Person
---bar name = if name == "Leif" then pure person else throwError err404
---bar :: (MonadIO m, MonadError ServantErr m) => AcidState PeopleDb -> Name -> m Person
---bar db name = liftIO (query db (LookupPerson name)) >>= maybeError err404
-
---type AcidIO e = MonadReader (AcidState (EventState e))
-
---type AcidIO event m = (MonadIO m, QueryEvent event, MonadReader (AcidState (EventState event)) m)
 type AcidIO event m = (MonadIO m, MonadReader (AcidState (EventState event)) m)
 
---liftedQuery :: (MonadIO m, QueryEvent event, MonadReader (AcidState (EventState event)) m) => event -> m (EventResult event)
 liftedQuery :: (QueryEvent event, AcidIO event m) => event -> m (EventResult event)
 liftedQuery q = do
   db <- ask
   liftIO $ query db q
 
---liftedUpdate :: AcidIO event m => event -> m (MethodResult event)
 liftedUpdate :: (UpdateEvent event, AcidIO event m) => event -> m (EventResult event)
 liftedUpdate q = do
   db <- ask
   liftIO $ update db q
-{-
-     AcidState (Data.Acid.Core.MethodState event)
-     -> event -> IO (Data.Acid.Core.MethodResult event)
--}
 
 peopleIndex :: PeopleServer PeopleIndex
 peopleIndex = liftedQuery AllPeople
 
 specificPerson :: PeopleServer SpecificPerson
---specificPerson = bar
---specificPerson db name = 
---specificPerson name = if name == "Leif" then pure person else throwError err404
 specificPerson name = liftedQuery (LookupPerson name) >>= maybeError err404
 
 updateSpecificPerson :: PeopleServer UpdateSpecificPerson
@@ -131,64 +105,24 @@ updateSpecificPerson name person = liftedUpdate (SetPerson name person) $> perso
 type ReaderHandler s = ReaderT s Handler
 type ReaderAcidHandler s = ReaderHandler (AcidState s)
 type PeopleServer api = ServerT api (ReaderAcidHandler PeopleDb)
+
 peopleAPI :: PeopleServer PeopleAPI
---peopleAPI = peopleIndex :<|> specificPerson
 peopleAPI =
   peopleIndex :<|>
   specificPerson :<|>
   updateSpecificPerson
 
---api :: Proxy PeopleAPI
---api = Proxy
+serveMonadStack :: HasServer api '[] => Proxy api -> (m ~> Handler) -> ServerT api m -> Application
+serveMonadStack p nt = serve p . hoistServer p nt
 
-app :: AcidState PeopleDb -> Application
---app db = serveApi @UpdateSpecificPerson Proxy (flip runReaderT db) updateSpecificPerson
-app db = serveApi @PeopleAPI Proxy (flip runReaderT db) peopleAPI
---app db = serve @PeopleAPI Proxy $ hoistServer @PeopleAPI Proxy (dbIOToHandler db) peopleAPI
-
---serveApi :: HasServer api '[] => Proxy api -> (forall x. m x -> Handler x) -> ServerT api m -> Application
-serveApi :: HasServer api '[] => Proxy api -> (forall x. m x -> Handler x) -> ServerT api m -> Application
-serveApi p nt = serve p . hoistServer p nt
+serveReader :: HasServer api '[] => Proxy api -> r -> ServerT api (ReaderHandler r) -> Application
+serveReader p r = serveMonadStack p (flip runReaderT r)
   
-
-{-
-serve
-  :: HasServer api '[] =>
-     Proxy api -> ServerT api Handler -> Application
-hoistServer
-  :: HasServer api '[] =>
-     Proxy api
-     -> (forall x. m x -> n x) -> ServerT api m -> ServerT api n
--}
-
---app db = serve @PeopleAPI Proxy $ hoistServer Proxy @PeopleAPI (dbIOToHandler db) peopleAPI
-
---dbIOToHandler :: (MonadIO m, QueryEvent event, MonadReader (AcidState (EventState event)) m) 
---dbIOToHandler :: (MonadIO m, IsAcidic db, MonadReader (AcidState db) m) => db -> m (Maybe a) -> Handler a
---dbIOToHandler :: (MonadIO m, IsAcidic db, MonadReader (AcidState db) m) => db -> m (Maybe a) -> Handler a
---dbIOToHandler db action = runReaderT db action
-
---dbIOToHandler :: (MonadIO m, MonadReader (AcidState PeopleDb) m) => AcidState PeopleDb -> m (a) -> Handler a
---dbIOToHandler db action = liftIO $ runReaderT action db
-
---dbIOToHandler :: AcidState PeopleDb -> ReaderT (AcidState PeopleDb) IO (Maybe a) -> Handler a
---dbIOToHandler :: AcidState PeopleDb -> AppM a -> Handler a
---readerToHandler state action = runReaderT action state
---dbIOToHandler db action = Handler $ ExceptT $ maybeToRight err404 <$> runReaderT action db
---dbIOToHandler db action = liftIO (runReaderT action db) >>= maybeError err404
-
---dbIOToHandler :: MonadIO m => AcidState PeopleDb -> ReaderT (AcidState PeopleDb) m (Maybe a) -> Handler a
---dbIOToHandler db action = (runReaderT action db) >>= maybeError err404
+app :: AcidState PeopleDb -> Application
+app db = serveReader @PeopleAPI Proxy db peopleAPI
 
 main :: IO ()
 main = bracket
-  (openLocalState (PeopleDb $ Map.fromList [("Leif", Person "Leif" 38)]))
+  (openLocalState (PeopleDb $ Map.fromList [(name samplePerson, samplePerson)]))
   (closeAcidState)
   (run 3000 . app)
-  
-{-  
-main = do
-  db <- openLocalState (PeopleDb $ Map.fromList [("Leif", Person "Leif" 38)])
-  run 3000 $ app db
-  closeAcidState db
--}
